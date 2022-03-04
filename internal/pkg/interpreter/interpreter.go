8eb036f24a55ddc1819c9959dd0b2aa50058636a
Mvmo <mvmogo@gmail.com>
2022-03-04 01:02:57 +0100

    Update Stack to SickObjectStack

--- internal/pkg/interpreter/interpreter.go
+++ internal/pkg/interpreter/interpreter.go
@@ -21,25 +21,25 @@ func NewInterpreter(instructions []instructions.Instruction, Labels *map[string]
 }
 
 func (interpreter Interpreter) Run() error {
-	var stack Stack
-	var storage map[string]types.SickType = make(map[string]types.SickType)
+	var objectStack SickObjectStack
+	var storage map[string]types.SickObject = make(map[string]types.SickObject)
 
 	for i := 0; i < len(interpreter.Instructions); i++ {
 		instruction := interpreter.Instructions[i]
 
 		switch instruction.OpCode {
 		case instructions.INS_IPUSH:
-			stack.Push(types.AnyToSickType(instruction.Params[0]))
+			objectStack.Push(types.AnyToSickObject(instruction.Params[0]))
 			continue
 		case instructions.INS_SPUSH:
-			stack.Push(types.AnyToSickType(instruction.Params[0]))
+			objectStack.Push(types.AnyToSickObject(instruction.Params[0]))
 			continue
 		case instructions.INS_BPUSH:
-			stack.Push(types.AnyToSickType(instruction.Params[0]))
+			objectStack.Push(types.AnyToSickObject(instruction.Params[0]))
 			continue
 		case instructions.INS_ADD:
-			val1 := stack.Pop()
-			val2 := stack.Pop()
+			val1 := objectStack.Pop()
+			val2 := objectStack.Pop()
 
 			if val1.TypeName() != val2.TypeName() {
 				return fmt.Errorf("can't invoke Add-Instruction with %v(%v) and %v(%v)", val1.ToHuman(), val1.TypeName(), val2.ToHuman(), val2.TypeName())
@@ -47,73 +47,73 @@ func (interpreter Interpreter) Run() error {
 
 			switch val1.(type) {
 			case types.SickString:
-				stack.Push(val2.(types.SickString).Value + val1.(types.SickString).Value)
+				objectStack.Push(val2.(types.SickString).Value + val1.(types.SickString).Value)
 			case types.SickInt:
-				stack.Push(val1.(types.SickInt).Value + val2.(types.SickInt).Value)
+				objectStack.Push(val1.(types.SickInt).Value + val2.(types.SickInt).Value)
 			}
 			continue
 		case instructions.INS_SUB:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsInt() - val1.AsInt())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsInt() - val1.AsInt())
 			continue
 		case instructions.INS_MUL:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsInt() * val1.AsInt())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsInt() * val1.AsInt())
 			continue
 		case instructions.INS_DIV:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsInt() / val1.AsInt())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsInt() / val1.AsInt())
 			continue
 		case instructions.INS_MOD:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsInt() % val1.AsInt())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsInt() % val1.AsInt())
 			continue
 		case instructions.INS_CMP:
-			val1 := stack.Pop()
-			val2 := stack.Pop()
-			stack.Push(val1 == val2)
+			val1 := objectStack.Pop()
+			val2 := objectStack.Pop()
+			objectStack.Push(val1 == val2)
 			continue
 		case instructions.INS_LT:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsFloat() < val1.AsFloat())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsFloat() < val1.AsFloat())
 			continue
 		case instructions.INS_GT:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsFloat() > val1.AsFloat())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsFloat() > val1.AsFloat())
 			continue
 		case instructions.INS_LTE:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsFloat() <= val1.AsFloat())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsFloat() <= val1.AsFloat())
 			continue
 		case instructions.INS_GTE:
-			val1 := stack.Pop().(types.SickNum)
-			val2 := stack.Pop().(types.SickNum)
-			stack.Push(val2.AsFloat() >= val1.AsFloat())
+			val1 := objectStack.Pop().(types.SickNum)
+			val2 := objectStack.Pop().(types.SickNum)
+			objectStack.Push(val2.AsFloat() >= val1.AsFloat())
 			continue
 		case instructions.INS_REQ:
 			requiredType := instruction.Params[0].(string)
-			typeOfStackHead := stack.Peek().TypeName()
+			typeOfSickObjectStackHead := objectStack.Peek().TypeName()
 
-			if typeOfStackHead != requiredType {
-				return fmt.Errorf("required type %v and got %v", requiredType, typeOfStackHead)
+			if typeOfSickObjectStackHead != requiredType {
+				return fmt.Errorf("required type %v and got %v", requiredType, typeOfSickObjectStackHead)
 			}
 			continue
 		case instructions.INS_STORE:
 			identifier := instruction.Params[0].(string)
-			toStore := stack.Pop()
+			toStore := objectStack.Pop()
 			storage[identifier] = toStore
 			continue
 		case instructions.INS_LOAD:
 			identifier := instruction.Params[0].(string)
 			toPush := storage[identifier]
-			stack.Push(toPush)
+			objectStack.Push(toPush)
 			continue
 		case instructions.INS_DEL:
 			identifier := instruction.Params[0].(string)
@@ -124,7 +124,7 @@ func (interpreter Interpreter) Run() error {
 			i = whereToJump - 1
 			continue
 		case instructions.INS_CJMP: // first param is where to jump if true and second where to jump if false
-			condition := stack.Pop().(types.SickBool)
+			condition := objectStack.Pop().(types.SickBool)
 			var whereToJump int
 			if condition.Value {
 				whereToJump = instruction.Params[0].(int) - 1
@@ -135,16 +135,16 @@ func (interpreter Interpreter) Run() error {
 			i = whereToJump
 			continue
 		case instructions.INS_DUP:
-			head := stack.Pop()
-			stack.Push(head)
-			stack.Push(head)
+			head := objectStack.Pop()
+			objectStack.Push(head)
+			objectStack.Push(head)
 			continue
 		case instructions.INS_PRINT:
-			head := stack.Pop()
+			head := objectStack.Pop()
 			fmt.Print(head.ToHuman())
 			continue
 		case instructions.INS_PRINTLN:
-			head := stack.Pop()
+			head := objectStack.Pop()
 			fmt.Println(head.ToHuman())
 			continue
 		case instructions.INS_GOTO:
@@ -152,13 +152,13 @@ func (interpreter Interpreter) Run() error {
 			i = (*interpreter.Labels)[labelName] - 1
 			continue
 		case instructions.INS_DUMP:
-			fmt.Printf("=== Stack Dump ===\n")
-			for i := len(stack); i > 0; i-- {
+			fmt.Printf("=== SickObjectStack Dump ===\n")
+			for i := len(objectStack); i > 0; i-- {
 				var anno string
-				if len(stack) == i {
+				if len(objectStack) == i {
 					anno = "   <-- head"
 				}
-				fmt.Printf("%v: %v%v\n", i, stack[i-1].ToHuman(), anno)
+				fmt.Printf("%v: %v%v\n", i, objectStack[i-1].ToHuman(), anno)
 			}
 			fmt.Printf("==================\n")
 			continue
